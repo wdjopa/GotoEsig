daf6427d3a8b7ac43038e613046f870fd11f9ebc
TANE TSAMO Friedrich Bryne <bftane@gmail.com>
2019-12-02 11:24:33 +0100

    connexion inscription views

---
+++ app/src/main/java/fr/tchatat/gotoesig/Inscription.java
@@ -0,0 +1,14 @@
+package fr.tchatat.gotoesig;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inscription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+    }
+}
--- app/src/main/java/fr/tchatat/gotoesig/MainActivity.java
+++ app/src/main/java/fr/tchatat/gotoesig/MainActivity.java
@@ -2,13 +2,29 @@ package fr.tchatat.gotoesig;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button inscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inscription = findViewById(R.id.btnRegister);
+        inscription.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == inscription){
+            Intent register = new Intent(this, Inscription.class);
+            startActivity(register);
+        }
     }
 }
