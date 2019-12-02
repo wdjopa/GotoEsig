8410679de0bf4e0e14c3a64c38304ac09be9179a
TANE TSAMO Friedrich Bryne <bftane@gmail.com>
2019-12-02 11:40:31 +0100

    background

---
+++ app/src/main/java/fr/tchatat/gotoesig/AjoutTrajet.java
@@ -0,0 +1,14 @@
+package fr.tchatat.gotoesig;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjoutTrajet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_trajet);
+    }
+}
