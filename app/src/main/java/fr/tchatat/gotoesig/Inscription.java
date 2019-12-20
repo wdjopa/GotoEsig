811cbfb376f005d709c4471591ff68c20ac7cd91
Wilfried <wilfried.djopa@gmail.com>
2019-12-20 11:24:46 +0100

    Début d'implémentation de firebase pour l'inscription

--- app/src/main/java/fr/tchatat/gotoesig/Inscription.java
+++ app/src/main/java/fr/tchatat/gotoesig/Inscription.java
@@ -1,14 +1,77 @@
 package fr.tchatat.gotoesig;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Inscription extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private String TAG = "Inscription";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+        mAuth = FirebaseAuth.getInstance();
+
+        Button inscriptionBtn = findViewById(R.id.btnValidateInscription);
+        inscriptionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = findViewById(R.id.etEmail).toString();
+                String pseudo = findViewById(R.id.etPseudo).toString();
+                String password = findViewById(R.id.etPass).toString();
+                String confirm = findViewById(R.id.etConfirm).toString();
+                if (!email.equals("") && !pseudo.equals("") && !password.equals("") && !confirm.equals("")) {
+                    if (password.equals(confirm)) {
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(Inscription.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            Log.d(TAG, "createUserWithEmail:success");
+                                            FirebaseUser user = mAuth.getCurrentUser();
+                                            Intent home = new Intent(Inscription.this, HomeActivity.class);
+                                            startActivity(home);
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                            Toast.makeText(Inscription.this, "Authentication failed.",
+                                                    Toast.LENGTH_SHORT).show();
+//                                            updateUI(null);
+                                        }
+
+                                        // ...
+                                    }
+                                });
+                    } else {
+
+                    }
+                } else {
+
+                }
+
+
+            }
+        });
+
     }
+
+    public void updateUI(FirebaseUser user) {
+
+
+    }
+
 }
--- app/src/main/java/fr/tchatat/gotoesig/MainActivity.java
+++ app/src/main/java/fr/tchatat/gotoesig/MainActivity.java
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth = new FirebaseAuth(FirebaseApp.getInstance());
 
     private Button inscription, connexion;
 
@@ -32,5 +42,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent home = new Intent(this, HomeActivity.class);
             startActivity(home);
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Gson gson = new Gson();
+        String currentUserJson = gson.toJson(currentUser);
+        if (currentUser != null) {
+
+            Intent home = new Intent(this, HomeActivity.class);
+            startActivity(home);
+            Toast.makeText(this, "Déjà connecté", Toast.LENGTH_SHORT).show();
+        }
+        Log.d("Connexion", currentUserJson);
+//        updateUI(currentUser);
     }
 }
