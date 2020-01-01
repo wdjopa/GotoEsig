eaca10ef2ba27f19b7a503cd633abd970e0ccc7f
Wilfried <wilfried.djopa@gmail.com>
2020-01-01 17:34:31 +0100

    Correction de bugs

--- app/src/main/java/fr/tchatat/gotoesig/ui/profile/profile/ProfileFragment.java
+++ app/src/main/java/fr/tchatat/gotoesig/ui/profile/profile/ProfileFragment.java
@@ -1,15 +1,24 @@
 package fr.tchatat.gotoesig.ui.profile.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
+import fr.tchatat.gotoesig.HomeActivity;
+import fr.tchatat.gotoesig.MainActivity;
 import fr.tchatat.gotoesig.R;
+import fr.tchatat.gotoesig.ui.roads.NouveauTrajetFragment;
 
 public class ProfileFragment extends Fragment {
 
@@ -29,4 +38,41 @@ public class ProfileFragment extends Fragment {
         });*/
         return root;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.drawer, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_update:
+                // User chose the "Settings" item, show the app settings UI...
+                Intent i = new Intent(getActivity(), UpdateProfileActivity.class);
+                startActivity(i);
+                /*Fragment fragment = new NouveauTrajetFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();*/
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
+    }
+*/
 }
---
+++ app/src/main/java/fr/tchatat/gotoesig/ui/profile/profile/UpdateProfileActivity.java
@@ -0,0 +1,16 @@
+package fr.tchatat.gotoesig.ui.profile.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.tchatat.gotoesig.R;
+
+public class UpdateProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_profile);
+    }
+}
