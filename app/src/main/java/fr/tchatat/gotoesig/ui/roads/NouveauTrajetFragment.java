e128c1fa65f064c42687a765dc6bdc55c1d49fcc
TANE TSAMO Friedrich Bryne <bftane@gmail.com>
2020-01-01 15:49:59 +0100

    update

--- app/src/main/java/fr/tchatat/gotoesig/ui/roads/NouveauTrajetFragment.java
+++ app/src/main/java/fr/tchatat/gotoesig/ui/roads/NouveauTrajetFragment.java
@@ -1,5 +1,7 @@
 package fr.tchatat.gotoesig.ui.roads;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -14,6 +17,7 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -30,7 +34,7 @@ import java.util.List;
 
 import fr.tchatat.gotoesig.R;
 
-public class NouveauTrajetFragment extends Fragment implements View.OnClickListener {
+public class NouveauTrajetFragment extends Fragment {
 
     private NouveauTrajetViewModel nouveauTrajetViewModel;
     private Spinner spinnerMoyen;
@@ -38,6 +42,7 @@ public class NouveauTrajetFragment extends Fragment implements View.OnClickListe
     private ConstraintLayout voiturelayout;
     private static List<String> moyens;
     private static ArrayAdapter<String> adapter1;
+    private Button btnVal;
 
     public void setSpinner(){
         adapter1 = new ArrayAdapter<String>(this.getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, moyens);
@@ -60,6 +65,7 @@ public class NouveauTrajetFragment extends Fragment implements View.OnClickListe
 
         voiturelayout = root.findViewById(R.id.voitureLayout);
         voiturelayout.setVisibility(ConstraintLayout.GONE);
+        btnVal = root.findViewById(R.id.ajoutTrajetBtn);
 
         FirebaseApp.initializeApp(this.getActivity());
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -103,11 +109,30 @@ public class NouveauTrajetFragment extends Fragment implements View.OnClickListe
             }
         });
 
+        btnVal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                // Add the buttons
+                builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+
+                builder.setMessage("Vous parcourerez 7 km et le trajet durera 17 min ! Voulez-vous choisir ce trajet ?\n Attention, cette action ne peut être annulée");
+
+                // Create the AlertDialog
+                AlertDialog dialog = builder.create();
+            }
+        });
+
         return root;
     }
 
-    @Override
-    public void onClick(View view) {
-
-    }
 }
