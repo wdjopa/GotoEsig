49c0eb081c70e32abdf6964af6dc2b8cc581afc2
Wilfried <wilfried.djopa@gmail.com>
2020-01-02 14:02:17 +0100

    liste des trajet - debut

--- app/src/main/java/fr/tchatat/gotoesig/ui/roads/MesTrajetsFragment.java
+++ app/src/main/java/fr/tchatat/gotoesig/ui/roads/MesTrajetsFragment.java
@@ -1,9 +1,12 @@
 package fr.tchatat.gotoesig.ui.roads;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,7 +17,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+
 import fr.tchatat.gotoesig.R;
+import fr.tchatat.gotoesig.models.Trajet;
+import fr.tchatat.gotoesig.models.User;
 
 public class MesTrajetsFragment extends Fragment {
 
@@ -24,6 +37,37 @@ public class MesTrajetsFragment extends Fragment {
         mesTrajetsViewModel = ViewModelProviders.of(this).get(MesTrajetsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_mes_trajets, container, false);
         getActivity().setTitle("Mes Trajets");
+
+        String uid = FirebaseAuth.getInstance().getUid();
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                 trajet = dataSnapshot.getValue(User.class);
+                Log.d("userStart", new Gson().toJson(user));
+
+                String []  etudiants = new String[db.getListEtudiants().size()];
+                int i = 0;
+                for(Etudiant e : db.getListEtudiants()){
+                    Log.e("data", e.toString());
+                    etudiants[i]  = e.getNoms();
+                    i++;
+                }
+
+                ListView lv = findViewById(R.id.listeView);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(ListEtudiants.this, android.R.layout.simple_list_item_1, etudiants);
+                lv.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("trajets", "loadUser:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("/users");
+        DatabaseReference usersRef = ref.child(uid + "/trajets");
+        usersRef.addValueEventListener(userListener);
 
         //final TextView textView = root.findViewById(R.id.text_gallery);
         /*
